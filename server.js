b9d87e30403bd70e029b6ea7a1472d1cb00c23ca
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-07-02 18:06:09 -0400

    update

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const mysql = require('mysql2');
+const fs = require('fs');
+const exp = require('constants');
 require('dotenv').config();
+
 
 
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.json());
+app.use('', express.static('public'));
+app.use('/admin', (req, res, next) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    console.log(username, password);
+
+    if (!username || !password) {
+        res.status(403).send("Unauthorized");
+    }
+
+    connection.query(
+        `call findAdmin('${username}', '${password}')`,
+        (err, results) => {
+            if (err || !username || !password || !results) {
+                res.status(403).send("Unauthorized");
+            } else if (results[0].length > 0) {
+                    console.log(results);
+                    next();
+            }
+        }
+    );
+});
+app.use('/admin', express.static('admin'));
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
@@ -67,6 +95,65 @@ app.post('/signup', (req, res) => {
     );
 });
 
+app.get('/admin/adminGetUser', (req, res) => {
+    const searchUsername = req.query.searchUsername;
+    const username = req.query.username;
+    const password = req.query.password;
+
+    connection.query(
+        `call findAdmin('${username}', '${password}')`,
+        (err, results) => {
+            if (err) {
+                res.send(err);
+            } else {
+                if (results.length > 0) {
+                    connection.query(
+                        `call findUser('${searchUsername}')`,
+                        (err, results) => {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                res.send(results);
+                            }
+                        }
+                    );
+                } else {
+                    res.send('Unauthorized');
+                }
+            }
+        }
+    )
+});
+
+app.post('/admin/adminDeleteUser', (req, res) => {
+    const searchUsername = req.body.searchUsername;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    connection.query(
+        `call findAdmin('${username}', '${password}')`,
+        (err, results) => {
+            if (err) {
+                res.send(err);
+            } else {
+                if (results.length > 0) {
+                    connection.query(
+                        `call deleteUser('${searchUsername}')`,
+                        (err, results) => {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                res.send(results);
+                            }
+                        }
+                    );
+                } else {
+                    res.send('Unauthorized');
+                }
+            }
+        }
+    )
+});
 
 const PORT = process.env.PORT;
 
